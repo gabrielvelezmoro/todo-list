ce1003a3c8bc8a0f7b1a27b59ac0f275837a0fc0
gabrielvelezmoro <gabrieuvelezmoro592@gmail.com>
2025-04-27 20:39:01 -0300

    refactor: lombok

--- src/main/java/br/com/gabrielvelezmoro/todolist/user/UserModel.java
+++ src/main/java/br/com/gabrielvelezmoro/todolist/user/UserModel.java
@@ -1,33 +1,12 @@
 package br.com.gabrielvelezmoro.todolist.user;
 
+import lombok.Data;
+
+@Data
 public class UserModel {
     private String username;
     private String name;
     private String password;
 
-    public void setUsername(String username){
-        this.username = username;
-    }
 
-    public String getUsername(){
-        return this.username;
-    }
-
-    public void setName(String name){
-        this.name = name;
-    }
-
-    public String getName(){
-        return this.name;
-    }
-
-
-    public void setPassword(String password){
-        this.password = password;
-    }
-
-
-    public String getPassword(){
-        return this.password;
-    }
 }
