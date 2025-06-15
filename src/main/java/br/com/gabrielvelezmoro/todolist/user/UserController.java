10a8fb052dfa62e5cb5de26bb93a9b07186496fd
gabrielvelezmoro <gabrieuvelezmoro592@gmail.com>
2025-06-15 15:01:17 -0300

    refactor: ResponseEntity to return HTTP status code and message

--- src/main/java/br/com/gabrielvelezmoro/todolist/user/UserController.java
+++ src/main/java/br/com/gabrielvelezmoro/todolist/user/UserController.java
@@ -1,6 +1,8 @@
 package br.com.gabrielvelezmoro.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +17,16 @@ public class UserController {
 
 
     @PostMapping("/")
-    public UserModel create(@RequestBody UserModel userModel){
+    public ResponseEntity create(@RequestBody UserModel userModel){
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if(user != null){
+            System.out.println("Usuario já existe!!");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario já existe");
+
+        }
+
         var userCreated = this.userRepository.save(userModel);
-        return userCreated;
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
--- src/main/java/br/com/gabrielvelezmoro/todolist/user/UserModel.java
+++ src/main/java/br/com/gabrielvelezmoro/todolist/user/UserModel.java
@@ -1,5 +1,6 @@
 package br.com.gabrielvelezmoro.todolist.user;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -15,6 +16,8 @@ public class UserModel {
     @Id
     @GeneratedValue(generator = "UUID")
     private UUID id;
+
+    @Column(unique = true)
     private String username;
     private String name;
     private String password;
