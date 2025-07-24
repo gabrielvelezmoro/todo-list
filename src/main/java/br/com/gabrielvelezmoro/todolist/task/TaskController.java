a90f8dabaa51d2d587805db6e11e3e34aad16f18
gabrielvelezmoro <gabrieuvelezmoro592@gmail.com>
2025-07-24 15:11:28 -0300

    Feat: Print my idUser that is getted by requestDotGetAttributes

--- src/main/java/br/com/gabrielvelezmoro/todolist/task/TaskController.java
+++ src/main/java/br/com/gabrielvelezmoro/todolist/task/TaskController.java
@@ -1,21 +1,24 @@
 package br.com.gabrielvelezmoro.todolist.task;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/tasks")
 public class TaskController {
 
+
+    @RestController
+@RequestMapping("/tasks")
 
         @Autowired
         private ITaskRepository taskRepository;
 
         @PostMapping("/")
-        public TaskModel create(@RequestBody TaskModel taskmodel){
+        public TaskModel create(@RequestBody TaskModel taskmodel, HttpServletRequest request){
+            System.out.println("Chegou no controller "+ request.getAttribute("idUser"));
            var task = this.taskRepository.save(taskmodel);
            return task;
         }
