24046c4b019adb2d39596c303e99e2a408e90c1e
Gabriel Velezmoro <gabrieuvelezmoro@gmail.com>
2025-08-08 16:08:17 -0300

    feat: validate date and time

--- src/main/java/br/com/gabrielvelezmoro/todolist/task/TaskController.java
+++ src/main/java/br/com/gabrielvelezmoro/todolist/task/TaskController.java
@@ -3,13 +3,16 @@ package br.com.gabrielvelezmoro.todolist.task;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@
-        RestController
+import java.time.LocalDateTime;
+
+@RestController
 @RequestMapping("/tasks")
 public class TaskController {
 
@@ -18,10 +21,16 @@ public class TaskController {
     private ITaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskmodel, HttpServletRequest request) {
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
         System.out.println("Chegou no controller " + request.getAttribute("idUser"));
-        var task = this.taskRepository.save(taskmodel);
-        return task;
+        var currentDate = LocalDateTime.now();
+        if(currentDate.isAfter(taskModel.getStartAt())){
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("A data de início deve ser maior do que a data atual");
+        }
+        var task = this.taskRepository.save(taskModel);
+        return ResponseEntity.status(HttpStatus.OK).body(task);
     }
 
 }
